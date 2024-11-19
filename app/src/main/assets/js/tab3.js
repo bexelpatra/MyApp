b4cf63319f4476457400f849788d4e98eeeb0e7f
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2024-11-19 17:26:49 +0900

    tab3 upated

--- app/src/main/assets/js/tab3.js
+++ app/src/main/assets/js/tab3.js
@@ -1,36 +1,40 @@
 console.log('tab3')
-document.onload = function(){
-    let searchTerm = document.getElementById('searchTerm')
-    console.log('123')
-    searchTerm.addEventListener('change',function(e){
-        console.log(e)
-    })
-}
-
-function renderItems() {
+function renderItems(items) {
     const tbody = document.getElementById('tableBody');
     tbody.innerHTML = '';
-
+    console.log(items)
     items.forEach((item, index) => {
         const tr = document.createElement('tr');
         tr.innerHTML = `
-            <td>${item.name}</td>
-            <td>${item.email}</td>
-            <td>
-                <button onclick="deleteItem(${index})" class="delete-btn">
-                    Delete
-                </button>
-            </td>
+            <td>${item}</td>
         `;
         tbody.appendChild(tr);
     });
 }
+
 function addItem() {
     const name = document.getElementById('searchTerm').value;
+    renderItems(name);
+}
 
-    if (name ) {
-        items.push({ name});
-        renderItems();
-        clearInputs();
+async function search() {
+    let date = document.getElementById("searchTerm").value
+    let param = {
+        searchTerm:date
     }
+    let list = await Android.reqSearch(JSON.stringify(param))
+    renderItems(list)
+    return list
+}
+// only number, length < 9
+function handleInput(el,value) {
+    // Remove any non-numeric characters
+    let cleanValue = value.replace(/[^0-9]/g, '');
+    if(cleanValue.length>8){
+        cleanValue = cleanValue.substring(0,8)
+    }
+    if (value !== cleanValue) {
+        el.value = cleanValue
+    }
+    search()
 }
