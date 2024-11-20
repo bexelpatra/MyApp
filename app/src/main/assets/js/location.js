316271d58485f19da4136a860efc68952c3776be
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2024-11-20 16:32:11 +0900

    map api seperated

--- app/src/main/assets/js/location.js
+++ app/src/main/assets/js/location.js
@@ -1,44 +1,25 @@
 let initFg = "Y";
 let map;
-let currentMarker;
+let currentMarker={};
 
-function initMap(locInfo){
-    if(initFg == 'Y'){
-        let currentLocation = JSON.parse(Android.getCurrentLocation());
 
-        // 지도 초기화
-        map = L.map('map').setView([currentLocation.lat, currentLocation.lon], 18);
+function initMap(divId){
+    let currentLocation = JSON.parse(Android.getCurrentLocation());
 
-        // OpenStreetMap 타일 레이어 추가
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
+    // 지도 초기화
+    let map = L.map(divId).setView([currentLocation.lat, currentLocation.lon], 18);
 
-        initFg = "N";
-
-    }else{
-        if(locInfo != null){
-            if (navigator.geolocation) {
-                let i = 1;
-                locInfo.forEach(location => {
-
-                    let popup = '방문 장소 ' + i + ' ( ' + location.time + ' )'
-                        + '<br>위도 : ' + location.lat + ' / 경도 : ' + location.lon
-                        + '<br>메모 : ' + location.memo;
-
-                    L.marker([location.lat, location.lon]).addTo(map).bindPopup(popup).openPopup();
-                    map.setView([location.lat, location.lon], 18).draggable(ture);
-                });
-            } else {
-                alert('Your browser does not support Geolocation.');
-            }
-        }
-    }
+    // OpenStreetMap 타일 레이어 추가
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+    
+    return map
 }
 
-function currentLocation(){
-    if(currentMarker != null){
-        map.removeLayer(currentMarker);
+function currentLocation(map,tab){
+    if(currentMarker[tab] != null){
+        map.removeLayer(currentMarker[tab]);
     }
 
     let currentLocation = JSON.parse(Android.getCurrentLocation());
@@ -46,7 +27,7 @@ function currentLocation(){
     let popup = '현재 위치 ' + ' ( ' + now(3) + ' )'
         + '<br>위도 : ' + currentLocation.lat + ' / 경도 : ' + currentLocation.lon;
 
-    currentMarker = L.marker([currentLocation.lat, currentLocation.lon], /*{draggable: true}*/ ).addTo(map).bindPopup(popup).openPopup();
+    currentMarker[tab] = L.marker([currentLocation.lat, currentLocation.lon], /*{draggable: true}*/ ).addTo(map).bindPopup(popup).openPopup();
 
     /*
     marker.on('dragend', function(event) {
