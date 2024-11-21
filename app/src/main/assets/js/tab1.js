785bf135b61d60247fb46c0d0ab3a4ebf80e9bae
bizui18 <66097247+bizui18@users.noreply.github.com>
2024-11-21 17:25:59 +0900

    tab1 update

--- app/src/main/assets/js/tab1.js
+++ app/src/main/assets/js/tab1.js
@@ -17,16 +17,6 @@ function tab1_markMap(){
         let tab1_markers = [];
         let locLength = locInfo.length;
 
-        let polyline = L.polyline([], {
-            color: '#654e9fb2'
-        }).arrowheads({
-            frequency: '100m',
-            yawn: 60,
-            size: '15px',
-            color: '#654e9fb2',
-            fill: true
-        }).addTo(tab1_map);
-
         let tab1_icon = L.divIcon({
             className: 'tab1-custom-icon',
             html: "<div class='tab1-marker-pin'></div><i class='tab1-material-icons'></i>",
@@ -34,20 +24,34 @@ function tab1_markMap(){
             iconAnchor: [15, 15]
         });
 
+        let tab1_line = [];
         for(let i=0; i<locLength; i++){
             let markerStr = '자동 저장 ' + (i + 1) + ' ( ' + locInfo[i].time + ' )'
                         + '<br>위도 : ' + locInfo[i].lat + ' / 경도 : ' + locInfo[i].lon;
 
             let tab1_marker = L.marker([locInfo[i].lat, locInfo[i].lon], {icon: tab1_icon}).addTo(tab1_map)/*.bindPopup(markerStr)*/;
 
+            tab1_line.push([locInfo[i].lat, locInfo[i].lon])
+
             tab1_markers.push(tab1_marker);
-            polyline.addLatLng(tab1_marker.getLatLng());
+/*            polyline.addLatLng(tab1_marker.getLatLng());
 
             tab1_marker.on('dragend', function(e) {
                 let index = tab1_markers.indexOf(this);
                 polyline.setLatLngs(tab1_markers.map(m => m.getLatLng()));
-            });
+            });*/
         }
+
+        let polyline = L.polyline([tab1_line], {
+            color: '#654e9fb2'
+        }).arrowheads({
+            frequency: 'allvertices',
+            yawn: 60,
+            size: '15px',
+            color: '#654e9fb2',
+            fill: true
+        }).addTo(tab1_map);
+
         fitMapToFarthestMarkers(tab1_markers)
     }
 }
@@ -61,10 +65,19 @@ function fitMapToFarthestMarkers(markers) {
         bounds.extend(marker.getLatLng());
     });
 
+    // 지도의 크기와 경계를 기반으로 최적의 줌 레벨 계산
+    let mapWidth = tab1_map.getSize().x;
+    let mapHeight = tab1_map.getSize().y;
+
+    let latSpan = bounds.getNorth() - bounds.getSouth();
+    let lonSpan = bounds.getEast() - bounds.getWest();
+
+    // 계산된 줌 레벨을 Leaflet의 최대 지원 줌 레벨(18)로 제한
+    let maxZoom = Math.min(18, tab1_map.getBoundsZoom(bounds));
     // 지도 뷰 조정
     tab1_map.fitBounds(bounds, {
         padding: [50, 50],
-        maxZoom: 12
+        maxZoom: maxZoom
     });
 }
 
