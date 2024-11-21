41c80baa9f308d9f7dd5749ff4040fcfaec6f786
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2024-11-21 11:25:13 +0900

    Update leafmap.js

--- app/src/main/assets/js/leafmap.js
+++ app/src/main/assets/js/leafmap.js
@@ -1,36 +1,36 @@
-let CURRENT_MAP_ID;
-let CURRENT_MARKER;
+// let CURRENT_MAP_ID;
+// let CURRENT_MARKER;
 
-function initMap(id){
-    let currenctMap = document.getElementById(CURRENT_MAP_ID);
-    if(id != CURRENT_MAP_ID){
-        currenctMap?.remove()
-    }
+// function initMap(id){
+//     let currenctMap = document.getElementById(CURRENT_MAP_ID);
+//     if(id != CURRENT_MAP_ID){
+//         currenctMap?.remove()
+//     }
 
-    let currentLocation = JSON.parse(Android.getCurrentLocation());
+//     let currentLocation = JSON.parse(Android.getCurrentLocation());
 
-    // 지도 초기화
-    map = L.map(id).setView([currentLocation.lat, currentLocation.lon], 18);
+//     // 지도 초기화
+//     map = L.map(id).setView([currentLocation.lat, currentLocation.lon], 18);
 
-    // OpenStreetMap 타일 레이어 추가
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
-    CURRENT_MAP_ID = id;
-}
+//     // OpenStreetMap 타일 레이어 추가
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution: '© OpenStreetMap contributors'
+//     }).addTo(map);
+//     CURRENT_MAP_ID = id;
+// }
 
-function currentLocation(){
-    if(currentMarker != null){
-        map.removeLayer(currentMarker);
-    }
+// function currentLocation(){
+//     if(currentMarker != null){
+//         map.removeLayer(currentMarker);
+//     }
 
-    let currentLocation = JSON.parse(Android.getCurrentLocation());
+//     let currentLocation = JSON.parse(Android.getCurrentLocation());
 
-    let popup = '현재 위치 ' + ' ( ' + now(3) + ' )'
-        + '<br>위도 : ' + currentLocation.lat + ' / 경도 : ' + currentLocation.lon;
+//     let popup = '현재 위치 ' + ' ( ' + now(3) + ' )'
+//         + '<br>위도 : ' + currentLocation.lat + ' / 경도 : ' + currentLocation.lon;
 
-    currentMarker = L.marker([currentLocation.lat, currentLocation.lon], /*{draggable: true}*/ ).addTo(map).bindPopup(popup).openPopup();
-    CURRENT_MARKER = currentMarker;
+//     currentMarker = L.marker([currentLocation.lat, currentLocation.lon], /*{draggable: true}*/ ).addTo(map).bindPopup(popup).openPopup();
+//     CURRENT_MARKER = currentMarker;
 
-    map.setView([currentLocation.lat, currentLocation.lon], 18);
-}
+//     map.setView([currentLocation.lat, currentLocation.lon], 18);
+// }
