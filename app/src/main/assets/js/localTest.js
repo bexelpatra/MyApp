69b24f7037d842f8fca804005825f8b023036696
bizui18 <66097247+bizui18@users.noreply.github.com>
2024-11-22 10:42:27 +0900

    test data

---
+++ app/src/main/assets/js/localTest.js
@@ -0,0 +1,14 @@
+let Android = {
+    save: function() {
+        let data = '[{"lat":"37.123","lon":"127.123","memo":"없음","option":"4","time":"2024-11-08  13:31:16"}]';
+        return data;
+    },
+    readFile: function() {
+        let data = '[{"lat":"37.123","lon":"127.123","memo":"없음","option":"4","time":"2024-11-08  13:31:16"}]';
+        return data;
+    },
+    getCurrentLocation: function() {
+        let data = '{"lat":"37.5628738","lon":"126.9775487"}';
+        return data;
+    }
+};
